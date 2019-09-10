02f53c423c1fbb67b2c0ddc5e5ad9ba947fe5f68
thecodersmacbookpro <mrdcharleston@gmail.com>
2019-09-09 21:17:35 -0700

    Added onClickLister to donut ImageView

--- app/src/main/java/com/example/droiddesserts/MainActivity.java
+++ app/src/main/java/com/example/droiddesserts/MainActivity.java
@@ -1,8 +1,12 @@
 package com.example.droiddesserts;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,27 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Toolbar myToolBar = findViewById(R.id.toolbar);
+        setSupportActionBar(myToolBar);
+
+        ImageView donutImageView = findViewById(R.id.donut_image_view);
+        donutImageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                displayToast(getString(R.string.donut_message));
+            }
+        });
+
+        
+
+
+
+    }
+
+
+    private void displayToast(String message) {
+        Toast toast = Toast.makeText(getApplicationContext(),message,Toast.LENGTH_SHORT);
+        toast.show();
     }
 }
